9d2c2227b012946ef57450ebbb5d7810158f5a64
Maksim Baranov <openok1989@mail.ru>
2015-11-19 08:18:13 +0300

    add tour spec.

--- test/admin/tours/tours.controller.spec.js
+++ test/admin/tours/tours.controller.spec.js
@@ -1,27 +1,45 @@
-// describe('AdminToursController', function() {
-  
-//   beforeEach(module('toursModule'));
-//   var $scope = {};
-//   var TourAPIUrl = 'https://api.parse.com/1/classes/tours/?include=country,hotel,place';
-  
-//   var httpBackend = null;
+describe('AdminToursController', function() {
+  beforeEach(module('toursModule'));
+  var $scope = {};
+  var tourAPIUrl = 'https://api.parse.com/1/classes/tours/?include=country,hotel,place';
+  var countryAPIUrl = 'https://api.parse.com/1/classes/countries'
+  var placeAPIUrl = 'https://api.parse.com/1/classes/places'
+  var hotelAPIUrl = 'https://api.parse.com/1/classes/hotels'
+  var $httpBackend = null;
+  var stubTour = null;
+  var stubHotel = null;
+  var stubCountry = null;
+  var stubPlace = null;
 
-//   beforeEach(inject(function($controller, $httpBackend) {
-//   	$controller('AdminToursController', {$scope: $scope});
-//   	httpBackend = $httpBackend;
-//   	// $httpBackend.whenGET(TourAPIUrl).respond(200);
-//   }));
+  beforeEach(inject(function($controller, _$httpBackend_) {
+  	$controller('AdminToursController', {$scope: $scope});
+  	$httpBackend = _$httpBackend_;
+  	stubHotel = {title: 'Test Hotel', stars: 5};
+  	stubCountry = {name: 'Belarus'};
+  	stubPlace = {name: 'Test Place'};
+  	stubTour = {title: 'Test tour', description: 'Test description', price: 2, objectId: 1,  country: stubCountry, hotel: stubHotel, place: stubPlace};
+    tourJsonResponse = JSON.stringify({results: [stubTour]});
+	$httpBackend.whenGET(countryAPIUrl).respond(200, JSON.stringify({results: [stubCountry]}));
+	$httpBackend.whenGET(placeAPIUrl).respond(200, JSON.stringify({results: [stubPlace]}));
+	$httpBackend.whenGET(hotelAPIUrl).respond(200, JSON.stringify({results: [stubHotel]}));
+  }));
 
-//   describe('initialize controller', function() {
-//   	it('sets pageName to Tours', function(){
-//   		expect($scope.pageName).toBe('Admin Tours');
-//   	});
+  describe('initialize', function() {
+    it('sets pageName to Tours', function() {
+	  expect($scope.pageName).toBe('Admin Tours');
+  	});
 
-//   	it('expect call to parse.com', function() {
-//   		httpBackend.expectGET(TourAPIUrl).respond(200);
-//   		httpBackend.verifyNoOutstandingExpectation();
-//   		// expect($httpBackend.verifyNoOutstandingExpectations).not.toThrow();
-//   	});
-//   });
+  	it('expects call to parse.com', function() {
+	  $httpBackend.expectGET(tourAPIUrl).respond(200);
+	  expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
+    });
 
-// });
+    it('sets countries, places, tours arrays', function() {
+  	  $httpBackend.expectGET(tourAPIUrl).respond(200, tourJsonResponse);
+  	  $httpBackend.flush();
+  	  expect(angular.equals($scope.tours[0], stubTour)).toBeTruthy();
+  	  expect(angular.equals($scope.places[0], stubPlace)).toBeTruthy();
+  	  expect(angular.equals($scope.countries[0], stubCountry)).toBeTruthy();
+    });	
+  });
+});
