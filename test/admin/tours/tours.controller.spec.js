3ef77f8a71e099832bf85ca9f260e97a9e7a759e
Maksim Baranov <openok1989@mail.ru>
2015-11-21 20:49:34 +0300

    Admin tour specs are completed.

--- test/admin/tours/tours.controller.spec.js
+++ test/admin/tours/tours.controller.spec.js
@@ -17,7 +17,7 @@ describe('AdminToursController', function() {
   	stubHotel = {title: 'Test Hotel', stars: 5, objectId: 1};
   	stubCountry = {name: 'Belarus', objectId: 1};
   	stubPlace = {name: 'Test Place', objectId: 1};
-  	stubTour = {title: 'Test tour', description: 'Test description', price: 2, objectId: 1,  country: stubCountry, hotel: stubHotel, place: stubPlace};
+  	stubTour = {title: 'Test tour', description: 'Test description', price: 2, objectId: 1,  country: stubCountry, hotel: stubHotel, place: stubPlace, duration: 20};
     tourJsonResponse = JSON.stringify({results: [stubTour]});
 	$httpBackend.whenGET(countryAPIUrl).respond(200, JSON.stringify({results: [stubCountry]}));
 	$httpBackend.whenGET(placeAPIUrl).respond(200, JSON.stringify({results: [stubPlace]}));
@@ -149,39 +149,112 @@ describe('AdminToursController', function() {
         expect($scope.tours[0].isModified).toBeTruthy();
       });
   	});
-
-  //   describe('$scope.update', function(){
-  //   beforeEach(function() {
-  //     $httpBackend.whenGET(hotelAPIUrl).respond(200, jsonResponse);
-  //     $httpBackend.flush();
-  //     $httpBackend.expectPUT(hotelAPIUrl + '/' + stubHotel.objectId).respond(200);      
-  //   });
-
-  //   it('expects PUT request to parse.com update point', function() {
-  //     $scope.update($scope.hotels[0]);
-  //     $httpBackend.flush();
-
-  //     expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
-  //     expect($httpBackend.verifyNoOutstandingRequest).not.toThrow();
-  //   });
-
-  //   it('expects hotel attributes are changed after PUT request', function() {
-  //     $scope.hotels[0].title = 'NewTitle';
-  //     $scope.hotels[0].stars = 5;
-      
-  //     $scope.update($scope.hotels[0]);
-  //     $httpBackend.flush();
-
-  //     expect($scope.hotels[0].title).toBe('NewTitle');
-  //     expect($scope.hotels[0].stars).toBe(5);
-
-  //     expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
-  //     expect($httpBackend.verifyNoOutstandingRequest).not.toThrow();
-  //   });
-  // });
-
-
-  	
   });
-  
+
+  describe('$scope.update', function(){
+    beforeEach(function() {
+      $httpBackend.expectGET(tourAPIUrl).respond(200, tourJsonResponse);
+      $httpBackend.flush();
+      $httpBackend.expectPUT('https://api.parse.com/1/classes/tours/1?include=country,hotel,place').respond(200);      
+      $httpBackend.expectGET('https://api.parse.com/1/classes/tours/1?include=country,hotel,place').respond(200, tourJsonResponse);	
+      $scope.tours[0].newPlaceId = stubPlace.objectId
+      $scope.tours[0].newCountryId = stubCountry.objectId
+      $scope.tours[0].newHotelId = stubHotel.objectId
+    });
+
+    it('expects PUT request to parse.com update point', function() {
+      $scope.update($scope.tours[0]);
+      $httpBackend.flush();
+
+      expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
+      expect($httpBackend.verifyNoOutstandingRequest).not.toThrow();
+    });
+
+    it('expects tour attributes are changed after PUT request', function() {
+      $scope.tours[0].title = 'NewTitle';
+      $scope.tours[0].description = 'New Description';
+      $scope.tours[0].price = 1000;
+      $scope.tours[0].duration = 50;
+      $scope.tours[0].place.objectId = 2;
+      $scope.tours[0].country.objectId = 2;
+      $scope.tours[0].hotel.objectId = 2;
+
+      $scope.update($scope.tours[0]);
+      $httpBackend.flush();
+
+      expect($scope.tours[0].title).toBe('NewTitle');
+      expect($scope.tours[0].description).toBe('New Description');
+      expect($scope.tours[0].price).toBe(1000);
+      expect($scope.tours[0].duration).toBe(50);
+      expect($scope.tours[0].place.objectId).toBe(2);
+      expect($scope.tours[0].country.objectId).toBe(2);
+      expect($scope.tours[0].hotel.objectId).toBe(2);
+      expect($scope.tours[0].isModified).toBeUndefined();
+
+      expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
+      expect($httpBackend.verifyNoOutstandingRequest).not.toThrow();
+    });
+  });
+
+  describe('$scope.cancelEdit', function() {
+    beforeEach(function() {
+      $httpBackend.whenGET(tourAPIUrl).respond(200, tourJsonResponse);
+      $httpBackend.flush();
+      $scope.edit(0, $scope.tours[0])
+    });
+
+    it('expects restore hotel attributes values', function() {
+	  $scope.tours[0].title = 'NewTitle';
+      $scope.tours[0].description = 'New Description';
+      $scope.tours[0].price = 1000;
+      $scope.tours[0].duration = 50;
+      $scope.tours[0].place.objectId = 2;
+      $scope.tours[0].country.objectId = 2;
+      $scope.tours[0].hotel.objectId = 2;
+
+      $scope.cancelEdit(0, $scope.tours[0])
+
+      expect($scope.tours[0].title).not.toBe('NewTitle');
+      expect($scope.tours[0].description).not.toBe('New Description');
+      expect($scope.tours[0].price).not.toBe(1000);
+      expect($scope.tours[0].duration).not.toBe(50);
+      expect($scope.tours[0].place.objectId).not.toBe(2);
+      expect($scope.tours[0].country.objectId).not.toBe(2);
+      expect($scope.tours[0].hotel.objectId).not.toBe(2);
+    });
+
+    it('sets hotel.isModified value to null', function() {
+      $scope.cancelEdit(0, $scope.tours[0])
+
+      expect($scope.hotels[0].isModified).toBeUndefined()
+    });
+  });
+
+  describe('$scope.cancelNewTour', function() {
+  	it('expects $scope.showForm value is true and attributes are null', function() {
+      $httpBackend.whenGET(tourAPIUrl).respond(200, tourJsonResponse);
+      $httpBackend.flush();
+      
+      $scope.new();
+      $scope.newTour.title = 'NewTitle';
+      $scope.newTour.description = 'New Description';
+      $scope.newTour.price = 1000;
+      $scope.newTour.duration = 50;
+      $scope.newTour.place.objectId = 2;
+      $scope.newTour.country.objectId = 2;
+      $scope.newTour.hotel.objectId = 2;
+
+      $scope.cancelNewTour();
+
+      expect($scope.showForm).toBeFalsy();
+      expect($scope.newTour.title).toBeNull();
+      expect($scope.newTour.description).toBeNull();
+      expect($scope.newTour.price).toBeNull();
+      expect($scope.newTour.duration).toBeNull();
+      expect($scope.newHotel.title).toBeNull();
+      expect($scope.newHotel.stars).toBeNull();
+      expect($scope.newTour.country.objectId).toBeNull();
+      expect($scope.newTour.place.objectId).toBeNull();
+    });
+  });
 });
